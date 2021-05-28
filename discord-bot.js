56b565f363dcae2558f1bf6c367c3a20469652c7
b3rtNATION <robifon@gmx.de>
2021-05-28 23:43:39 +0200

    added advanced channel management

--- discord-bot.js
+++ discord-bot.js
@@ -3,58 +3,74 @@ require("dotenv").config();
 const bot = new Eris(process.env.token, { restMode: true });
 
 const SUPPORTROOM = "841449567790170112";
-
-const SUPPORTROLE = "847811693252837406"
+const SUPPORTROLE = "847811693252837406";
+const MEMBERROLE = "841431597206863876";
+const EVERYONE = "841429444647976961";
+const FRIENDWITH = "841432902117490709"
+const FRIENDWITHOUT = "841433371728412673"
 
 const GUILD_ID = "841429444647976961";
 
 const CS = {
   parent: "841462843731738635",
   overwatch: "841464217776160788",
-  name: "CS:GO Matchroom",
+  name: "CS:GO",
+  count: 1,
 };
 const APEX = {
   parent: "847217713654661190",
   overwatch: "847218203158511676",
-  name: "APEX Matchroom",
+  name: "APEX",
+  count: 1
 };
 const COD = {
   parent: "847213945064390666",
   overwatch: "847214787356393472",
-  name: "COD Matchroom",
+  name: "COD",
+  count: 1
 };
 const LOL = {
   parent: "841797724223045632",
   overwatch: "841798341112889395",
-  name: "LOL Matchroom",
+  name: "LOL",
+  count: 1
 };
 const RL = {
   parent: "841463201518452756",
   overwatch: "841464253331406868",
-  name: "RL Matchroom",
+  name: "RL",
+  count: 1
 };
 const OTHER = {
   parent: "847466636834242581",
   overwatch: "847466932689043518",
   name: "Gamingroom",
+  count: 1
 };
 const GAMES = [CS, APEX, COD, LOL, RL, OTHER];
 const tempChannels = [];
+
+// ------------- E V E N T S -------------
 
 bot.on("voiceChannelSwitch", (member, newChannel, oldChannel) => {
   checkForChannelRequest(newChannel, member);
   checkMemberCountInTempChannel();
   checkForSupport(newChannel, member);
+  handleChannelPermission(newChannel, member, 'join')
 });
 
 bot.on("voiceChannelJoin", (member, newChannel) => {
   checkForChannelRequest(newChannel, member);
   checkForSupport(newChannel, member);
+  handleChannelPermission(newChannel, member, 'join')
 });
 
 bot.on("voiceChannelLeave", (member, oldChannel) => {
   checkMemberCountInTempChannel();
+  handleChannelPermission(oldChannel, member, 'leave')
 });
+
+// ------------- L O G I C -------------
 
 const checkForChannelRequest = (channel, member) => {
   for (const game of GAMES) {
@@ -64,13 +80,13 @@ const checkForChannelRequest = (channel, member) => {
   }
 };
 
-const checkForSupport = async (channel, member) => {
+const checkForSupport = (channel, member) => {
   if (SUPPORTROOM === channel.id) {
     const guilds = bot.guilds;
     guilds.forEach((guild) => {
       guild.members.forEach((admin) => {
         if (admin.roles.includes(SUPPORTROLE)) {
-          contactAdmins(admin.id, member)
+          contactAdmins(admin.id, member);
         }
       });
     });
@@ -88,21 +104,33 @@ const moveMemberToRoom = (newChannel, member) => {
 
 const createNewChannel = async (game, member) => {
   try {
-    const newChannel = await bot.createChannel(
+    const newVoiceChannel = await bot.createChannel(
       GUILD_ID,
-      `${game.name}`,
+      `${game.name} Matchroom #${game.count}`,
       2,
       {
-        nsfw: false,
-        reason: "new channel needed",
-        topic: "ingame talk",
         parentID: game.parent,
-        bitrate: 96000
+        bitrate: 96000,
       }
     );
-    tempChannels.push(newChannel.id);
-    moveMemberToRoom(newChannel, member);
-    console.log(newChannel);
+
+    const newTextChannel = await bot.createChannel(
+      GUILD_ID,
+      `${game.name}-matchroom-${game.count}`,
+      0,
+      {
+        parentID: game.parent,
+        permissionOverwrites: [
+          { deny: 8589934591, id: MEMBERROLE, type: "role" },
+          { deny: 8589934591, id: EVERYONE, type: "role" },
+          { deny: 8589934591, id: FRIENDWITH, type: "role" },
+          { deny: 8589934591, id: FRIENDWITHOUT, type: "role" },
+        ],
+      }
+    );
+    tempChannels.push({ voice: newVoiceChannel.id, text: newTextChannel.id, gameName: game.name  });
+    moveMemberToRoom(newVoiceChannel, member);
+    game.count++;
   } catch (err) {
     console.log(err);
   }
@@ -110,18 +138,38 @@ const createNewChannel = async (game, member) => {
 
 const checkMemberCountInTempChannel = () => {
   for (const channel of tempChannels) {
-    const members = bot.getChannel(channel).voiceMembers;
+    const members = bot.getChannel(channel.voice).voiceMembers;
     if (members.size === 0) {
       removeChannel(channel);
     }
   }
 };
 
+const handleChannelPermission = (channel, member, type) => {
+  if (type === 'join') {
+    for (const room of tempChannels) {
+      if (room.voice === channel.id) {
+        bot.getChannel(room.text).editPermission(member.id, 2953313361, 8, 'member')
+      }
+    }
+  } else {
+    for (const room of tempChannels) {
+      if (room.voice === channel.id) {
+        bot.getChannel(room.text).editPermission(member.id, 1, 8589934591, 'member')
+      }
+    }
+  }
+};
+
 const removeChannel = (channel) => {
-  bot.deleteChannel(channel);
+  bot.deleteChannel(channel.voice);
+  bot.deleteChannel(channel.text);
   const channelToDeleteIndex = tempChannels.findIndex(
-    (channelInArray) => channelInArray === channel
+    (channelInArray) => channelInArray.voice === channel.voice
   );
+  const gameName = tempChannels[channelToDeleteIndex].gameName
+  GAMES.find(game => game.name === gameName).count--
+
   tempChannels.splice(channelToDeleteIndex, 1);
 };
 
