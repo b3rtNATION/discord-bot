0323025704caef65fa77852ad027d88daaecbcc5
b3rtNATION <robifon@gmx.de>
2021-06-01 13:15:25 +0200

    added dynamic room number creation

--- discord-bot.js
+++ discord-bot.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const bot = new Eris(process.env.token, { restMode: true });
 
 const SUPPORTROOM = "841449567790170112";
+const ROB = "718887341300908093"
 
 const SUPPORTROLE = "847811693252837406";
 const MEMBERROLE = "841431597206863876";
@@ -14,37 +15,37 @@ const GUILD_ID = "841429444647976961";
 const TALK = {
   parent: "841459514116014100",
   overwatch: "847805213096149012",
-  name: "Talk"
-}
+  channelCount: 3,
+};
 const CS = {
   parent: "841462843731738635",
   overwatch: "841464217776160788",
-  name: "CS:GO Matchroom",
+  channelCount: 2,
 };
 const APEX = {
   parent: "847217713654661190",
   overwatch: "847218203158511676",
-  name: "APEX Matchroom",
+  channelCount: 2,
 };
 const COD = {
   parent: "847213945064390666",
   overwatch: "847214787356393472",
-  name: "COD Matchroom",
+  channelCount: 2,
 };
 const LOL = {
   parent: "841797724223045632",
   overwatch: "841798341112889395",
-  name: "LOL Matchroom",
+  channelCount: 2,
 };
 const RL = {
   parent: "841463201518452756",
   overwatch: "841464253331406868",
-  name: "RL Matchroom",
+  channelCount: 2,
 };
 const OTHER = {
   parent: "847466636834242581",
   overwatch: "847466932689043518",
-  name: "Gamingroom",
+  channelCount: 1,
 };
 const GAMES = [CS, APEX, COD, LOL, RL, OTHER, TALK];
 const tempChannels = [];
@@ -73,9 +74,9 @@ bot.on("guildMemberAdd", (guild, member) => {
   handleNewUser(member);
 });
 
-bot.on ("messageCreate", (msg) => {
-  handleMaxUserRequest(msg)
-})
+bot.on("messageCreate", (msg) => {
+  handleMaxUserRequest(msg);
+});
 
 // ------------- L O G I C -------------
 
@@ -93,6 +94,12 @@ const checkForSupport = (channel, member) => {
     guilds.forEach((guild) => {
       guild.members.forEach((admin) => {
         if (admin.roles.includes(SUPPORTROLE)) {
+          if (
+            admin.clienStatus.web === "offline" &&
+            admin.clienStatus.desktop === "offline" &&
+            admin.clienStatus.mobile === "offline"
+          )
+            return;
           contactAdmins(admin.id, member);
         }
       });
@@ -101,19 +108,29 @@ const checkForSupport = (channel, member) => {
 };
 
 const contactAdmins = async (admin, member) => {
-  const chatroom = await bot.getDMChannel(admin);
-  chatroom.createMessage(`${member.username} braucht dich als Support`);
+  try {
+    const chatroom = await bot.getDMChannel(admin);
+    chatroom.createMessage(`${member.username} braucht dich als Support`);
+  } catch (err) {
+    sendErrorMessage(err)
+  }
+  
 };
+
+const sendErrorMessage = (msg) => {
+  bot.getDMChannel(ROB).createMessage(msg)
+}
 
 const moveMemberToRoom = (newChannel, member) => {
   bot.editGuildMember(GUILD_ID, member.id, { channelID: newChannel.id });
 };
 
 const createNewChannel = async (game, member) => {
+  const roomNumber = getRoomNumber(game);
   try {
     const newVoiceChannel = await bot.createChannel(
       GUILD_ID,
-      game.name,
+      `Raum ${roomNumber}`,
       2,
       {
         parentID: game.parent,
@@ -123,7 +140,7 @@ const createNewChannel = async (game, member) => {
 
     const newTextChannel = await bot.createChannel(
       GUILD_ID,
-      game.name,
+      `Raum ${roomNumber}`,
       0,
       {
         parentID: game.parent,
@@ -140,13 +157,41 @@ const createNewChannel = async (game, member) => {
       voiceName: newVoiceChannel.name,
       text: newTextChannel.id,
       gameName: game.name,
-      owner: member.id
+      owner: member.id,
     });
     moveMemberToRoom(newVoiceChannel, member);
   } catch (err) {
-    console.log(err);
+    sendErrorMessage(err)
   }
 };
+
+const getRoomNumber = (game) => {
+  const category = bot.getChannel(game.parent)
+  if (category.channels.size % 2 === 0) {
+    return category.channels.size - (category.channels.size / 2)
+  }
+  return category.channels.size - ((category.channels.size - 1) / 2)
+}
+
+const handleChannelName = () => {
+  tempChannels.forEach(async (channel, index) => {
+    try {
+      if (!channel.voiceName.includes(index.toString())) {
+        const voiceChannel = bot.getChannel(channel.voice)
+        const textChannel = bot.getChannel(channel.text)
+        if (voiceChannel.name.toLowerCase().includes('max')) {
+          const userLimit = voiceChannel.userLimit
+          await voiceChannel.edit({ name: `Raum ${index + 1} MAX${userLimit}`})
+        } else {
+          await voiceChannel.edit({ name: `Raum ${index + 1}`})
+        }
+        await textChannel.edit({ name: `Raum ${index + 1}`})
+      }
+    } catch (err) {
+      sendErrorMessage (err)
+    }
+  })
+}
 
 const checkMemberCountInTempChannel = () => {
   for (const channel of tempChannels) {
@@ -178,33 +223,49 @@ const handleChannelPermission = (channel, member, type) => {
 };
 
 const handleNewUser = async (member) => {
-  if (member.roles.length === 0) {
-    const dm = await bot.getDMChannel(member.id);
-    dm.createMessage(
-      `Hallo ${member.username} und Willkommen auf dem Discord von desiRe Gaming!\n\nFalls Du hier neu bist, komm doch einfach mal in das Support Wartezimmer. Unsere Supporter sind schnellstmöglich für dich da!`
-    );
+  try {
+    if (member.roles.length === 0) {
+      const dm = await bot.getDMChannel(member.id);
+      dm.createMessage(
+        `Hallo ${member.username} und Willkommen auf dem Discord von desiRe Gaming!\n\nFalls Du hier neu bist, komm doch einfach mal in das Support Wartezimmer. Unsere Supporter sind schnellstmöglich für dich da!`
+      );
+    }
+  } catch (err) {
+    sendErrorMessage(err)
   }
 };
 
 const handleMaxUserRequest = (msg) => {
   for (const channel of tempChannels) {
-    if (channel.text === msg.channel.id && msg.author.id === channel.owner && msg.content.toLowerCase().startsWith('max')) {
-      const maxUser = msg.content.substring(3, msg.content.lenth)
+    if (
+      channel.text === msg.channel.id &&
+      msg.author.id === channel.owner &&
+      msg.content.toLowerCase().startsWith("max")
+    ) {
+      const maxUser = msg.content.substring(3, msg.content.lenth);
       bot.getChannel(channel.voice).edit({
         name: `${channel.voiceName} MAX${maxUser}`,
-        userLimit: maxUser
-      })
+        userLimit: maxUser,
+      });
     }
   }
-}
+};
 
-const removeChannel = (channel) => {
-  bot.deleteChannel(channel.voice);
-  bot.deleteChannel(channel.text);
+const removeChannel = async (channel) => {
+  try {
+    await bot.deleteChannel(channel.voice);
+    await bot.deleteChannel(channel.text);
+  } catch (err) {
+    sendErrorMessage(err)
+  }
+  
   const channelToDeleteIndex = tempChannels.findIndex(
     (channelInArray) => channelInArray.voice === channel.voice
   );
   tempChannels.splice(channelToDeleteIndex, 1);
+  setTimeout(() => {
+    handleChannelName();
+  }, 500);
 };
 
 bot.connect();
